62f20248db53ded3f878466d463b05848699c5ac
Mercy <miheretgirmachew@gmail.com>
2024-05-06 14:48:51 +0300

    row

--- netflix-clone/src/components/Banner/Banner.js
+++ netflix-clone/src/components/Banner/Banner.js
@@ -1,36 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import axios from "../../utils/axios"
-import requests from "../../utils/request"
-import "./banner.css"
-
+import React, { useEffect, useState } from 'react';
+import axios from "../../utils/axios";
+import requests from "../../utils/requests";
+import "./banner.css";
 
 const Banner = () => { 
     const [movie, setMovie] = useState({});
-    useEffect(()=>{
+    useEffect(() => {
         (async () => {
-            try{
-                const request = await axios.get(requests.fetchNetflixOrginals)
+            try {
+                const request = await axios.get(requests.fetchNetflixOrginals);
                 setMovie(request.data.results[
                     Math.floor(Math.random() * request.data.results.length)
                 ]);
             } catch (error) {
-                console.log("error", error)
+                console.log("error", error);
             }
-        }) ()
+        })();
     }, []);
 
-    function truncate(str,n){
-        return str?.length > n ? str.substr(0, n-1) + "..." : str;
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
     }
-  return (
-    <div
-        className="banner"
-        style={{
-            backgroundSize: "cover",
-            backgroundImage: "url('https://image.tmdb.org/t/p/orginal${movie?.backdrop_path}')",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat"
-        }}
+
+    return (
+        <div
+            className="banner"
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat"
+            }}
         >
             <div className='banner__contents'>
                 <h1 className='banner__title'>
@@ -40,11 +40,11 @@ const Banner = () => {
                     <button className='banner__button play'>Play</button>
                     <button className='banner__button'>My List</button>
                 </div>
-                <h1 className='banner__description'>{truncate(movie?.overview,150)}</h1>
+                <h1 className='banner__description'>{truncate(movie?.overview, 150)}</h1>
             </div>
-            <div className='banner__fadeBottom'/>
-    </div>
-  )
-}
+            <div className='banner__fadeBottom' />
+        </div>
+    );
+};
 
-export default Banner
+export default Banner;
