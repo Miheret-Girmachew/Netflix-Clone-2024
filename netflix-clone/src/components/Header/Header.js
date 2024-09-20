ea10357e188479338b871dbb0d1612824c05425d
Miheret Girmachew <124549518+Miheret-Girmachew@users.noreply.github.com>
2024-09-19 18:24:22 -0800

    finalizing

--- netflix-clone/src/components/Header/Header.js
+++ netflix-clone/src/components/Header/Header.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import "./header.css";
 import NetflixLogo from "../../assets/images/pngwing.com.png";
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Header = () => {
   const [scrolling, setScrolling] = useState(false);
+  const navigate = useNavigate(); // Initialize the navigate hook
 
   useEffect(() => {
     const handleScroll = () => {
@@ -25,31 +27,35 @@ const Header = () => {
     };
   }, []);
 
+  const handleLogout = () => {
+    navigate('/login'); 
+  };
+
   return (
-      <div className={`header_outer_container ${scrolling ? 'nav__black' : ''}`}>
-          <div className='header_container'>
-              <div className='header_left'>
-                  <ul>
-                      <li><img src={NetflixLogo} alt="Netflix Logo" width="100" /></li>
-                      <li>Home</li>
-                      <li>TVShows</li>
-                      <li>Movies</li>
-                      <li>Latest</li>
-                      <li>MyList</li>
-                      <li>Browse by Languages</li>
-                  </ul>
-              </div>
-              <div className='header_right'>
-                  <ul>
-                      <li><SearchIcon /></li>
-                      <li><NotificationsNoneIcon /></li>
-                      <li><AccountBoxIcon /></li>
-                      <li><ArrowDropDownIcon /></li>
-                  </ul>
-              </div>
-          </div> 
+    <div className={`header_outer_container ${scrolling ? 'nav__black' : ''}`}>
+      <div className='header_container'>
+        <div className='header_left'>
+          <ul>
+            <li><img src={NetflixLogo} alt="Netflix Logo" width="100" /></li>
+            <li>Home</li>
+            <li>TVShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className='header_right'>
+          <ul>
+            <li><SearchIcon /></li>
+            <li><NotificationsNoneIcon /></li>
+            <li><AccountBoxIcon /></li>
+            <li onClick={handleLogout} style={{ cursor: 'pointer' }}><LogoutIcon /></li> 
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,18 +1,36 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
-import Banner from '../../components/Banner/Banner'
-import RowList from '../../components/Rows/RowList/RowList'
+import React, { useState, useEffect } from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import Banner from '../../components/Banner/Banner';
+import RowList from '../../components/Rows/RowList/RowList';
+import { ThreeDots } from 'react-loader-spinner'; 
+import './Home.css';
 
 const Home = () => {
-    return (
-        <>
-            <Header />
-            <Banner />
-            <RowList />
-            <Footer />
-        </>
-    )
-}
+    const [isLoading, setIsLoading] = useState(true);
 
-export default Home
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000); 
+    }, []);
+
+    return (
+        <div className="whole">
+            {isLoading ? (
+                <div className="loader-container">
+                    <ThreeDots color="#ffffff" height={80} width={80} />
+                </div>
+            ) : (
+                <>
+                    <Header />
+                    <Banner />
+                    <RowList />
+                    <Footer />
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Home;
