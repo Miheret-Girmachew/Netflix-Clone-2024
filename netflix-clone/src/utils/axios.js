a369aed32837585e2935970327402cb63ca7223f
Miheret Girmachew <124549518+Miheret-Girmachew@users.noreply.github.com>
2024-09-21 21:30:08 -0800

    finalizing

--- netflix-clone/src/utils/axios.js
+++ netflix-clone/src/utils/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3",
+  baseURL: "https://api.themoviedb.org/3",
+  headers: {
+    Authorization: `Bearer your_access_token_here`, // Use a bearer token if applicable
+  },
 });
 
 export default instance;
